e27f5fc201f240ef5186644cc7439a26e1d756e5
ManuelFreire-rgb <mfreire.bernal@gmail.com>
2025-07-17 00:52:20 +0000

    Trabajo Finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,52 @@
 import React from "react";
+import { useState } from 'react'
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+
+
+export default function Home() {
+	const [color, establishColor] = useState("red");
+    const [colors, establishColores] = useState(["red", "yellow", "green"]);
+  
+    const alternarColor = () => {
+      const currentIndex = colors.indexOf(color);
+      const nextIndex = (currentIndex + 1) % colors.length;
+      establishColor(colors[nextIndex]);
+    };
+  
+    const addPurple = () => {
+      if (!colors.includes("purple")) {
+        establishColores([...colors, "purple"]);
+      }
+    };
+
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container justify-content-center my-3">
+            <div className="row my-1">
+                <div className="col-md-12 p-2 rounded-3 bg-secondary d-flex justify-content-center">
+                    <button type="button" className="btn btn-primary mx-2" onClick={alternarColor}>Cambiar color</button>
+                    <button type="button" className="btn btn-success mx-2" onClick={addPurple}>Añadir Purpura</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12 rounded-3 bg-light d-flex flex-column align-items-center p-4">
+                    <div className="bg-dark p-3 rounded-3">
+                        {colors.map((c) => (
+                            <div
+                                key={c}
+                                className={`brillo ${c} ${color === c ? "active" : ""}`}
+                                onClick={() => establishColor(c)}
+                            />
+                        ))}
+                    </div>
+                    <div className="palo">
+                    </div>
+                </div>
+            </div>
+        </div>
 	);
-};
+}
 
-export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { useState } from 'react'
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,10 +10,55 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+
+const Home = () =>  {
+  const [color, establishColor] = useState("red");
+    const [colors, establishColores] = useState(["red", "yellow", "green"]);
+  
+    const alternarColor = () => {
+      const currentIndex = colors.indexOf(color);
+      const nextIndex = (currentIndex + 1) % colors.length;
+      establishColor(colors[nextIndex]);
+    };
+  
+    const addPurple = () => {
+      if (!colors.includes("purple")) {
+        establishColores([...colors, "purple"]);
+      }
+    };
+
+  return (
+    <div className="container justify-content-center my-3">
+            <div className="row my-1">
+                <div className="col-md-12 p-2 rounded-3 bg-secondary d-flex justify-content-center">
+                    <button type="button" className="btn btn-primary mx-2" onClick={alternarColor}>Cambiar color</button>
+                    <button type="button" className="btn btn-success mx-2" onClick={addPurple}>Añadir Purpura</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12 rounded-3 bg-light d-flex flex-column align-items-center p-4">
+                    <div className="bg-dark p-3 rounded-3">
+                        {colors.map((c) => (
+                            <div
+                                key={c}
+                                className={`brillo ${c} ${color === c ? "active" : ""}`}
+                                onClick={() => establishColor(c)}
+                            />
+                        ))}
+                    </div>
+                    <div className="palo">
+                    </div>
+                </div>
+            </div>
+        </div>
+  );
+}
+
+export default Home
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
   </React.StrictMode>,
 )
+
